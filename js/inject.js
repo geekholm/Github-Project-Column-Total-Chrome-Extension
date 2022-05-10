f53c934a276236b52341727926f50b02ecacbb20
Niklas Ekholm <niklas@niklasekholm.com>
2022-05-11 00:20:40 +0300

    tiny tweak

--- js/inject.js
+++ js/inject.js
@@ -3,26 +3,26 @@
 
   var columnheaders = document.querySelectorAll('[role=columnheader]');
   for (let i = 0; i < columnheaders.length; i++) {
-    if (columnheaders[i].firstElementChild.firstChild.innerText == 'Estimate (hours)') {
+    if (columnheaders[i].firstElementChild.firstChild.innerText == 'h') {
       desiredIndex = i;
     }
   }
 
-  var estimateTotal = 0;
+  var hTotal = 0;
   var rows = document.querySelectorAll('[role=row]');
   for (let i = 0; i < rows.length; i++) {
     if (rows[i].children[desiredIndex] && rows[i].children[desiredIndex].firstElementChild && rows[i].children[desiredIndex].firstElementChild.firstChild) {
       var thing = parseFloat(rows[i].children[desiredIndex].firstElementChild.firstChild.innerText);
       if (!isNaN(thing)) {
-        estimateTotal = estimateTotal + thing;
+        hTotal = hTotal + thing;
       }
     }
   }
-  alert(estimateTotal);
+  alert(hTotal);
   // var aChild = document.createElement("p");
   // aChild.classList.add("fiDMsB");
   // aChild.style.marginLeft = '3px';
-  // var total = document.createTextNode(estimateTotal);
+  // var total = document.createTextNode(hTotal);
   // aChild.appendChild(total);
   // columnheaders[desiredIndex].firstElementChild.firstChild.appendChild(aChild);
 })();
